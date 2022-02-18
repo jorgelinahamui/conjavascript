6aa13a9dcd6ccbb3a13dd66834c5ab34967abab0
Jorgelina Hamui <jorgelinahamui12@gmail.com>
2022-02-17 21:58:56 -0300

    Empezando con Javascript

---
+++ js/condicionales.js
@@ -0,0 +1,13 @@
+
+console.log('Empezando con condicionales')
+
+let hora = prompt ('Que hora es en donde estas?')
+
+if (hora < 13) {
+    alert('Buenos dias')
+} else if (hora < 20) {
+    alert('Buenas tardes')
+}
+else {
+    alert('Buenas noches')
+}
---
+++ js/for.js
@@ -0,0 +1,20 @@
+console.log ('Empezamos con el for');
+
+ /* for (let i = 1; i <= 10 ; i = i++ ) {
+    console.log ('iteracion: ', i);
+
+} */
+
+let number = prompt ('Dame un numero')
+
+for (let i = 1; i<= 20; i++) {
+    const result = number * i
+    // 5 x 1 = 5
+    // 5 x 2 = 10
+    // console.log (number + 'x' + i + '=' + result);
+    //console.log(`${number} x ${i}= ${result}` );
+
+    console.log(`es es el ciclo ${i} `)
+
+
+}
